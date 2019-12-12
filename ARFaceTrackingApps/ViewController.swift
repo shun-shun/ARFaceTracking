5fb0da64fb4bbd0ca7ed7d54885b62ce1a265d31
権藤　俊 <gondo.syun0711@gmail.com>
2019-12-12 20:50:43 +0900

    Initial Commit

---
+++ ARFaceTrackingApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARFaceTrackingApps
+//
+//  Created by しゅん on 2019/12/12.
+//  Copyright © 2019 g-chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
